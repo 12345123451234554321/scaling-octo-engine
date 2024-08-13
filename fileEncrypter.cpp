d5a74e3cb44c531c0b297260f69374ad3bf0df0a
12345123451234554321 <150269688+12345123451234554321@users.noreply.github.com>
2024-08-13 20:38:49 +0500

    Add files via upload

---
+++ fileEncrypter.cpp
@@ -0,0 +1,57 @@
+#include <fstream>
+#include <iostream>
+#include <cmath>
+#include <cstdint>
+
+using namespace std;
+
+const uint64_t key = 1;
+
+inline int16_t getSize(int64_t size) {
+	if(size < 8) {return size;} else {return 8;}
+}
+ 
+int32_t main(int32_t argc, char const *argv[])
+{	
+	fstream infile ("Desert.jpg", fstream::binary | fstream::in);
+	fstream outfile ("DesertEnc.jpg", fstream::binary | fstream::out);
+
+	infile.seekg(0, ios::end);
+	int64_t size = infile.tellg();
+	infile.seekg(0);
+	char* buffer = new char[8];
+	uint64_t buff = 0;
+	int64_t lastSize = size;
+	cout << size << endl;
+	for(uint64_t i = 0; i < ceil((long double)size / 8.0); i++) {
+		if(lastSize < 8) {
+			infile.read(buffer, lastSize);
+		} else {
+			infile.read(buffer, 8);
+		}
+		
+		for(uint64_t j = 0; j < getSize(lastSize); j++)
+		{
+			buff |= (long long)buffer[j] << (j * 8);
+		}
+		
+		buff ^= key;
+
+		for(uint64_t j = 0; j < getSize(lastSize); j++)
+		{
+			buffer[i] = (long long)buff >> (j * 8);
+		}
+		
+		if(lastSize < 8) {
+			outfile.write(buffer, lastSize);
+		} else {
+			outfile.write(buffer, 8);
+		}
+
+		lastSize -= 8;
+	}
+	delete[] buffer;
+
+	system("pause");
+	return 0;
+}
